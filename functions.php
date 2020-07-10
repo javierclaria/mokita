7554b3e33e20542bc47d818b2f18fe2b313c33a7
javierclaria <javier.claria@gmail.com>
2020-07-10 14:26:11 -0300

    add fonts and Styles

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 function mokita_enqueue_styles() {
  $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
- wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version')
- );
+ wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
+ wp_enqueue_style( 'google-font','https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"', false );
 }
 add_action( 'wp_enqueue_scripts', 'mokita_enqueue_styles' );
 
