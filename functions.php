d9209ac90658e13286bb50ca778d3784e8b83e20
javierclaria <javier.claria@gmail.com>
2020-07-11 17:38:35 -0300

    add

--- functions.php
+++ functions.php
@@ -30,4 +30,25 @@ function mokita_header() {
     echo 'Testing';
 }
 
+
+// Add Custom Blocks
+add_action('acf/init', 'my_acf_init');
+
+function my_acf_init() {
+	
+	// check function exists
+	if( function_exists('acf_register_block') ) {
+		
+		// register a testimonial block
+		acf_register_block(array(
+			'name'				=> 'sliderhomepage',
+			'title'				=> __('Slider del Homepage'),
+			'description'		=> __('Slider Custom para el hoomepage'),
+			'render_callback'	=> 'my_acf_block_render_callback',
+			'category'			=> 'formatting',
+			'icon'				=> 'admin-comments',
+			'keywords'			=> array('slider','homepage'),
+		));
+	}
+}
 ?>
