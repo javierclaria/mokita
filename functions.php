a0797ef35e610efdb23043fe4d99cad0efa2a987
javierclaria <javier.claria@gmail.com>
2020-10-01 14:00:18 -0300

    change functions

--- functions.php
+++ functions.php
@@ -163,23 +163,23 @@ function my_acf_init() {
 		}
 }
 
-function custom_pre_get_posts_query( $q ) {
+// function custom_pre_get_posts_query( $q ) {
 
-	$tax_query = (array) $q->get( 'tax_query' );
+// 	$tax_query = (array) $q->get( 'tax_query' );
 
-	$tax_query[] = array(
-		   'taxonomy' => 'product_cat',
-		   'field' => 'slug',
-		   'terms' => array( 'rebajas' ), // Don't display products in the clothing category on the shop page.
-		   'operator' => 'NOT IN'
-	);
-
-
-	$q->set( 'tax_query', $tax_query );
-
-}
+// 	$tax_query[] = array(
+// 		   'taxonomy' => 'product_cat',
+// 		   'field' => 'slug',
+// 		   'terms' => array( 'rebajas' ), // Don't display products in the clothing category on the shop page.
+// 		   'operator' => 'NOT IN'
+// 	);
 
 
-add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' );  
+// 	$q->set( 'tax_query', $tax_query );
+
+// }
+
+
+// add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' );  
 
 ?>
